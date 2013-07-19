a4bea989c56bfd5d459e193b24e3d40e20a667e0
Jen Fong-Adwent <jen@ednapiranha.com>
2013-07-19 15:08:33 -0400

    switched to buttons

--- js/main.js
+++ js/main.js
@@ -4,6 +4,45 @@ define(['jquery', 'base/calendar'],
 
   var wrapper = $('#wrapper');
   var ul = $('#calendar');
+  var buttons = '';
+
+  var buttonObj = [
+    {
+      type: 'period',
+      css: 'period light red',
+      name: 'light'
+    },
+    {
+      type: 'period',
+      css: 'period medium red',
+      name: 'medium'
+    },
+    {
+      type: 'period',
+      css: 'period dark red',
+      name: 'heavy'
+    },
+    {
+      type: 'symptom',
+      css: 'symptom bloated',
+      name: 'bloated'
+    },
+    {
+      type: 'symptom',
+      css: 'symptom cramps',
+      name: 'cramps'
+    },
+    {
+      type: 'symptom',
+      css: 'symptom bloated',
+      name: 'bloated'
+    }
+  ];
+
+  for (var i = 0; i < buttonObj.length; i ++) {
+    buttons += '<button class="hidden ' + buttonObj[i].css +
+               '" data-action="tag">' + buttonObj[i].name + '</button>';
+  }
 
   for (var i = 1; i < calendar.daysInMonth + 1; i ++) {
     var currClass = '';
@@ -11,12 +50,12 @@ define(['jquery', 'base/calendar'],
     if (i === calendar.currentDay) {
       currClass = 'today';
     }
-    ul.append($('<li class="' + currClass + '">' + i +
-      '<textarea class="hidden"></textarea></li>'));
+
+    ul.append($('<li class="' + currClass + '">' + i + buttons + '</li>'));
   }
 
   ul.on('touchstart click', 'li', function (ev) {
-    $('textarea').addClass('hidden');
-    $(this).find('textarea').removeClass('hidden');
+    $('button').addClass('hidden');
+    $(this).find('button').removeClass('hidden');
   });
 });
