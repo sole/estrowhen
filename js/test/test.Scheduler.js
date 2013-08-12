709d38dceee3eaa1e740a1987ad9aeff039c857c
Jennifer Fong <jen@ednapiranha.com>
2013-08-12 17:06:24 -0400

    more tests

--- js/test/test.Scheduler.js
+++ js/test/test.Scheduler.js
@@ -1,23 +1,51 @@
-define(['jquery', '../base/Scheduler'],
-  function ($, Scheduler) {
+define(['jquery', '../base/Scheduler', 'asyncStorage'],
+  function ($, Scheduler, asyncStorage) {
   'use strict';
 
   var s = new Scheduler();
-  var button = $('button.medium');
+  var buttonP = $('button.medium');
+  var buttonS = $('button.tired');
+  var id = 'date-2013-7-12';
+  var marker = buttonP.closest('li').find('.marker');
 
   describe('Scheduler', function () {
     after(function () {
       asyncStorage.clear();
     });
 
-    it('should set period', function () {
-      s.setPeriod(button);
-      expect(button.hasClass('active')).to.equal(true);
+    it('should set period', function (done) {
+      setTimeout(function () {
+        s.setPeriod(buttonP);
+        expect(buttonP.hasClass('active')).to.equal(true);
+        expect(marker.hasClass('medium')).to.equal(true);
+        asyncStorage.getItem('period:' + id, function (d) {
+          expect(d).to.equal('medium');
+          done();
+        });
+      }, 100);
     });
 
-    it('should unset period', function () {
-      s.setPeriod(button);
-      expect(button.hasClass('active')).to.equal(false);
+    it('should unset period', function (done) {
+      s.setPeriod(buttonP);
+      expect(buttonP.hasClass('active')).to.equal(false);
+      expect(marker.hasClass('medium')).to.equal(false);
+      setTimeout(function () {
+        asyncStorage.getItem('period:' + id, function (d) {
+          expect(d).to.equal(null);
+          done();
+        });
+      }, 100);
+    });
+
+    it('should set symptom', function (done) {
+      s.setSymptom(buttonS);
+      expect(buttonS.hasClass('active')).to.equal(true);
+      setTimeout(function () {
+        asyncStorage.getItem('symptoms:' + id, function (d) {
+          expect(d.indexOf('tired')).to.equal(0);
+          done();
+        });
+      }, 100);
     });
   });
 });
